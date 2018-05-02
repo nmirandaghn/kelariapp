050d7a08c9cc86640c9b03b39c4b780dcff7676b
Nelson Miranda <nmirandaghn@gmail.com>
2018-05-01 20:49:45 -0600

    Advanced login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180422165045) do
+ActiveRecord::Schema.define(version: 20180501215251) do
 
   create_table "users", force: :cascade do |t|
     t.string "user_name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20180422165045) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["user_name"], name: "index_users_on_user_name", unique: true
   end
