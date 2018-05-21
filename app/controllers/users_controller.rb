7f427787c83c6ca009c2e85d1fc7147effc1009f
Nelson Miranda <nmirandaghn@gmail.com>
2018-05-20 21:37:42 -0600

    Reset password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ DEFAULT_PASSWORD = 'inicio'.freeze
 class UsersController < ApplicationController
   before_action :logged_in_user
   before_action :administrator, except: [:change_password, :update_password]
-  before_action :correct_user, only: [:change_password, :update_password]
+  # before_action :correct_user, only: [:change_password, :update_password]
 
   def show
     @user = User.find(params[:id])
@@ -40,21 +40,30 @@ class UsersController < ApplicationController
     end
   end
 
-  def change_password; end
+  def reset
+    @user = User.find(params[:user_id])
+    @user.password = DEFAULT_PASSWORD
+    @user.password_confirmation = DEFAULT_PASSWORD
 
-  def change_password
-    if user_params[:password] != user_params[:password_confirmation]
-      flash[:success] = 'Passwords are not equal'
-      render 'change_password'
-    end
-    @user.password = user_params[:password]
-    @user.password_confirmation = user_params[:password_confirmation]
-    if @user.save
-      flash[:success] = 'Password was succesfully changed'
-      redirect_to root_url
-    end
-    render 'change_password'
+    flash[:success] = @user.save ? 'Password reseted' : 'Error'
+    redirect_to edit_user_path(@user)
   end
+
+  # def change_password; end
+
+  # def change_password
+  #   if user_params[:password] != user_params[:password_confirmation]
+  #     flash[:success] = 'Passwords are not equal'
+  #     render 'change_password'
+  #   end
+  #   @user.password = user_params[:password]
+  #   @user.password_confirmation = user_params[:password_confirmation]
+  #   if @user.save
+  #     flash[:success] = 'Password was succesfully changed'
+  #     redirect_to root_url
+  #   end
+  #   render 'change_password'
+  # end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,12 @@ Rails.application.routes.draw do
   get  '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  # post '/reset', to: 'users#reset'
 
   resources :users do
     get 'change_password'
     post 'change_password'
+    post '/reset', to: 'users#reset'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
